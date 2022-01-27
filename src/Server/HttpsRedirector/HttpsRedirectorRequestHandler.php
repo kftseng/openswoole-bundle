15fcd770d743d0663ea76bed76da3e415956ed3a
Julian Haupt <julian@haupt.media>
2022-01-27 21:56:28 +0100

    include get parameters in redirection

--- src/Server/HttpsRedirector/HttpsRedirectorRequestHandler.php
+++ src/Server/HttpsRedirector/HttpsRedirectorRequestHandler.php
@@ -12,8 +12,13 @@ final class HttpsRedirectorRequestHandler implements RequestHandlerInterface
 {
     public function handle(Request $request, Response $response): void
     {
-        if(isset($request->header['host'])) {
-            $response->header('Location', 'https://' . $request->header['host'] . $request->server['path_info']);
+        if (isset($request->header['host'])) {
+            $url = "https://" . $request->header['host'] . $request->server['path_info'];
+
+            if (!empty($request->get))
+                $url .= "?" . http_build_query($request->get);
+
+            $response->header('Location', $url);
             $response->status(301);
         } else {
             $response->status(500);
